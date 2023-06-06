43cb6921ae12fb25dc5e2c7955de602c2a887c23
dxcbl008 <131538604+dxcbl008@users.noreply.github.com>
2023-06-06 23:09:00 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "老北鼻AI智能助手-XCBL.CC",
+  title: "老北鼻AI智能助手-lbbai.com",
   description: "您的私人ChatGPT聊天机器人。",
   viewport: {
     width: "device-width",
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "老北鼻AI智能助手-XCBL.CC",
+    title: "老北鼻AI智能助手-lbbai.com",
     statusBarStyle: "default",
   },
 };
